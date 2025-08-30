0c03cb37b8eaab0ef10439d6771fce8147b187b8
duong <isdon243@gmail.com>
2025-08-30 19:36:53 +0700

    fix-theme

--- FrontEnd-NextJS/pet-service/src/providers/theme.provider.tsx
+++ FrontEnd-NextJS/pet-service/src/providers/theme.provider.tsx
@@ -4,9 +4,9 @@ import { ReactNode } from "react";
 export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
     <NextThemesProvider
-      attribute="class" // thêm class 'dark' lên <html>
-      defaultTheme="system" // theo hệ điều hành
-      enableSystem
+      attribute="class"
+      defaultTheme="light"
+      enableSystem={false}
     >
       {children}
     </NextThemesProvider>
